06b0c0d7dd3cdfc04ce0be8fe1730d37909d8e60
Herre <jaherrerias@gmail.com>
2019-02-16 13:11:17 +0100

    <form 			onSubmit={event => { 				event.preventDefault() 				if (!user.name || !user.username) return
    				props.addUser(user)
    				setUser(initialFormState)
    			}}
    		>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
+import EditUserForm from './forms/EditUserForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ];
+
+	const [ users, setUsers ] = useState(usersData);
+	
+	const addUser = user => {
+		user.id = users.length + 1;
+		setUsers([ ...users, user ]);
+	};
+
+	const deleteUser = id => {
+		setEditing(false);
+		setUsers(users.filter(user => user.id !== id));
+	};
+
+	const [ editing, setEditing ] = useState(false);
+	const initialFormState = { id: null, name: '', username: '' };
+	const [ currentUser, setCurrentUser ] = useState(initialFormState);
+
+	const editMode = user => {
+		setEditing(true);
+	
+		setCurrentUser({ id: user.id, name: user.name, username: user.username });
+	};
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false);
+	
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+	}
+
+  return (
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{
+						editing ? (
+							<div>
+								<h2>Edit user</h2>
+								<EditUserForm
+									editing={editing}
+									setEditing={setEditing}
+									currentUser={currentUser}
+									updateUser={updateUser}
+								/>
+							</div>
+						) : (
+							<div>
+								<h2>Add user</h2>
+								<AddUserForm addUser={addUser} />
+							</div>
+						)
+					}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editMode={editMode} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+  );
+};
 
 export default App;
---
+++ src/tables/UserTable.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const UserTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <div class="flex-row">
+                <button
+                  className="button btn-row muted-button"
+                  onClick={() => props.editMode(user)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="button btn-row muted-button"
+                  onClick={() => props.deleteUser(user.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default UserTable;
